b46324b420b88146415791d71c869535d8a1d8f1
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2021-02-08 19:49:58 -0400

    refactor: rename test method

--- FizzBuzzTestCase.cs
+++ FizzBuzzTestCase.cs
@@ -5,7 +5,7 @@ namespace learn_tdd_fizzbuzz
   public class FizzBuzzTestCase
   {
     [Fact]
-    public void Test_Something()
+    public void Test_Can_Instantiate()
     {
       var fizzbuzz = new FizzBuzz();
       Assert.NotNull(fizzbuzz);
