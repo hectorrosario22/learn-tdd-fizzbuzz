61c3c6feca21c3b04bdda10ffdd45dbe8d833f49
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2021-02-08 20:03:36 -0400

    green: writing a working test

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -1,7 +1,12 @@
+using System.Linq;
+
 namespace learn_tdd_fizzbuzz
 {
   public class FizzBuzz
   {
-
+    public int[] Generate()
+    {
+      return Enumerable.Range(0, 100).ToArray();
+    }
   }
 }
