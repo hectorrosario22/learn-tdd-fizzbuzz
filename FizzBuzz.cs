474a4e35d3f734206cff653588195112862957ab
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2021-02-08 20:25:06 -0400

    green: writing a working test

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -11,6 +11,12 @@ namespace learn_tdd_fizzbuzz
       var numbers = new string[100];
       for (var index = 0; index < numbers.Length; index++)
       {
+        if ((index + 1) == 3)
+        {
+          numbers[index] = "Fizz";
+          continue;
+        }
+
         numbers[index] = $"{index + 1}";
       }
       return numbers;
